47532b08927b9e3cad9b181df66744c0a2f23da0
rsacharias <rudolfsacharias@posteo.de>
2025-04-27 11:19:23 +0200

    fix: Fix display of result
    After the game is finished the result is no longer visible.

--- js/rock-paper-scissor.js
+++ js/rock-paper-scissor.js
@@ -1,7 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const maxPoints = 5;
+const maxPoints = 2;
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
@@ -18,16 +18,26 @@ function getComputerChoice() {
 
 function update_score() {
   const score = document.querySelector("#score");
-  score.textContent = `You: ${humanScore} -- Comp: ${computerScore}`;
+  score.textContent = `You: ${humanScore} -- Cpu: ${computerScore}`;
 
   if (humanScore == maxPoints || computerScore == maxPoints) {
-    const resultGame = document.querySelector("#result-game");
+    const resultGame = document.createElement("div");
+    resultGame.id = "result-game";
 
     resultGame.textContent =
-      humanScore >= 3 ? "Congrats. You've won!" : "Uff ... You lose!";
+      humanScore < maxPoints ? "Uff ... You lose!" : "Congrats. You've won!";
+
+    document.body.appendChild(resultGame);
 
     humanScore = 0;
     computerScore = 0;
+  }
+}
+
+function clear_result() {
+  if (humanScore == 0 && computerScore == 0) {
+    const resultGame = document.querySelector("#result-game");
+    if (resultGame) resultGame.remove();
   }
 }
 
@@ -39,6 +49,8 @@ function playRound(event) {
 
   const capitalize = (word) =>
     word[0].toUpperCase() + word.slice(1).toLowerCase();
+
+  clear_result();
 
   if (humanChoice === computerChoice) {
     resultRound.textContent = "Tie!";
